ccf8bba56c2df2bc0bd95ec69d964286799eb352
Quinn Pollock <contact@quinnpollock.net>
2020-08-16 00:52:54 -0400

    inital commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Random Watchlist Picker</h1>
+    <Film />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Film from "./components/Film.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Film,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Film.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="hello">
+    <div class="input">
+      <label>Usernames seperate multiple with ,</label>
+      <input type="text" v-model="users" />
+      <button v-on:click="submit()">Submit</button>
+    </div>
+    <div v-if="loading">
+      <h2>Loading Film</h2>
+      <p>Sorry this may take a bit as we scrape letterboxd do to api restrictions</p>
+    </div>
+    <div v-else-if="pressed">
+      <div v-if="notfound">
+        <h2>Nothing Found</h2>
+        <p>Sorry nothing was found is your watchlist empty</p>
+      </div>
+      <div v-else id="hello">
+        <img v-bind:src="img_url" alt="image or film" />
+        <a v-bind:href="url">{{ name }}</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Film",
+  data() {
+    return {
+      users: "",
+      info: "",
+      notfound: false,
+      loading: false,
+      pressed: false,
+    };
+  },
+  methods: {
+    submit() {
+      this.pressed = true;
+      this.loading = true;
+      let userlist = this.users.split(",");
+      console.log(userlist);
+      let url = "https://letterboxd-random.ue.r.appspot.com/film?";
+      for (let i = 0; i < userlist.length; i++) {
+        if (i > 0) {
+          url += "&";
+        }
+        url += `users=${userlist[i].trim()}`;
+      }
+      try {
+        let vue = this;
+        console.log(url);
+        fetch(url)
+          .then(function (res) {
+            vue.loading = false;
+            if (res.status != 200) {
+              vue.notfound = true;
+              return "";
+            }
+
+            return res.json();
+          })
+          .then((json) => (this.info = json));
+      } catch (e) {
+        this.$alert(
+          "Something went wrong. Please try again in a moment. Error:" + e,
+          "An error occured"
+        );
+      }
+    },
+  },
+  computed: {
+    filmNotFound: function () {
+      return this.notfound;
+    },
+    url: function () {
+      return this.info.slug;
+    },
+    img_url: function () {
+      return this.info.image_url;
+    },
+    name: function () {
+      return this.info.film_name;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
