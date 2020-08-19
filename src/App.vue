7a399ade53475723ae430dbf04ad6512c4edc83b
Quinn Pollock <contact@quinnpollock.net>
2020-08-18 22:27:04 -0400

    convert to tabs

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,40 @@
 <template>
-  <div id="app">
-    <div id="main">
-      <Film />
-    </div>
-    <Footer />
-  </div>
+	<div id="app">
+		<div id="main">
+			<Film />
+		</div>
+		<Footer />
+	</div>
 </template>
 
 <script>
 import Film from "./components/Film.vue";
 import Footer from "./components/Footer.vue";
 export default {
-  name: "App",
-  components: {
-    Film,
-    Footer,
-  },
+	name: "App",
+	components: {
+		Film,
+		Footer,
+	},
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 30px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 30px;
 }
 
 #main {
-  min-height: -webkit-calc(75vh - 15px);
+	min-height: -webkit-calc(75vh - 15px);
 }
 
 ::selection {
-  background: #40bcf4;
-  color: white;
+	background: #40bcf4;
+	color: white;
 }
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,28 +1,30 @@
 <template>
-  <footer><a href="http://goodbyte.ca/">Goodbyte</a> &copy; 2020</footer>
+	<footer>
+		<a href="http://goodbyte.ca/">Goodbyte</a> &copy; 2020
+	</footer>
 </template>
 
 <script>
 export default {
-  name: "Footer",
+	name: "Footer",
 };
 </script>
 
 <style scoped>
 footer {
-  text-align: center;
-  margin: 8rem auto 2rem;
-  opacity: 0.6;
-  font-size: 12px;
-  letter-spacing: 0.03em;
+	text-align: center;
+	margin: 8rem auto 2rem;
+	opacity: 0.6;
+	font-size: 12px;
+	letter-spacing: 0.03em;
 }
 
 a {
-  color: inherit;
-  text-decoration: none;
+	color: inherit;
+	text-decoration: none;
 }
 
 a:hover {
-  text-decoration: underline;
+	text-decoration: underline;
 }
 </style>
