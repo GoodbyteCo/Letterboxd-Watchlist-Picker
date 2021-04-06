ba53d38daa63f77106dee16719f064aeef219fc0
Jack Guinane <jackguinane@gmail.com>
2021-04-05 22:37:45 -0700

    prepare parent

--- src/App.vue
+++ src/App.vue
@@ -23,51 +23,70 @@ export default {
 </script>
 
 <style>
-@media (prefers-reduced-motion) {
-	* {
-		animation: none !important;
-		transition: none !important;
+	@media (prefers-reduced-motion) 
+	{
+		* 
+		{
+			animation: none !important;
+			transition: none !important;
+		}
 	}
-}
-/* #toggle {
-	position: fixed;
-	top: 0;
-	right: 0;
-	z-index: 999;
-} */
 
-body {
-	background: #fff;
-	transition: background ease-in-out 0.25s;
-}
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	transition: color ease-in-out 0.25s;
-	margin-top: 30px;
-}
+	:root
+	{
+		--background: #fff;
+		--foreground: #2c3e50;
+		--primary: #1caff2;
+		--secondary: #000;
+		--tertiary: #fff;
+	}
 
-#main {
-	min-height: calc(85vh - 15px);
-	min-height: -webkit-calc(85vh - 15px);
-	min-height: -moz-calc(85vh - 15px);
-}
+	.dark
+	{
+		--background: #14181d;
+		--foreground: #76a0ca; /* #778fa6; */
+		--primary: #1caff2;
+		--secondary: #fff;
+		--tertiary: #fff;
+	}
 
-body.dark {
-	background: #14181d;
-	transition: background ease-in-out 0.25s;
-}
+	body 
+	{
+		background: var(--background);
+		transition: background ease-in-out 0.25s;
+	}
 
-.dark #app {
-	color: #778fa6;
-	transition: color ease-in-out 0.25s;
-}
+	#app 
+	{
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		color: var(--foreground);
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
 
-::selection {
-	background: #40bcf4;
-	color: white;
-}
+		transition: color ease-in-out 0.25s;
+		margin-top: 30px;
+	}
+
+	main 
+	{
+		min-height: calc(85vh - 15px);
+		min-height: -webkit-calc(85vh - 15px);
+		min-height: -moz-calc(85vh - 15px);
+
+		transform: translateY(40px);
+		transition: transform 1.2s cubic-bezier(0.82, 0.01, 0.45, 1);
+	}
+
+	.entered main,
+	.done main
+	{
+		transform: none;
+	}
+
+	::selection 
+	{
+		background: var(--primary);
+		color: var(--tertiary);
+	}
 </style>
