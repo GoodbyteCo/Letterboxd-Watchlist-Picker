2746b413db597fbc9d5a841686a63cbfb120673b
Jack Guinane <jackguinane@gmail.com>
2025-10-30 19:50:14 -0700

    Parse data from node API

--- api/letterboxd.ts
+++ api/letterboxd.ts
@@ -1,5 +1,6 @@
 import {Query} from './query'
 import {NoResultReason, Result} from './result'
+import * as cheerio from 'cheerio'
 
 export async function fetchFilm(query: Query): Promise<Result> {
 	const base = 'https://watchlistpicker.com/api'
@@ -46,15 +47,10 @@ export async function findFilm(query: Query): Promise<Result> {
 			return buildError(NoResultReason.UNKNOWN)
 		}
 
-		const html = await response.text()
-		// todo: parse?
-
-		return {
-			status: 'success',
-			title: html,
-			img:'https://a.ltrbxd.com/resized/sm/upload/8g/5p/p4/6b/8YWirGQidtZeSEmhqvQM5FrI6N1-0-230-0-345-crop.jpg?v=10e3695a0f',
-			url: letterboxdUrl.toString(),
-		}
+		return await response.text()
+			.then(cheerio.load) // parse into virtual DOM
+			.then(extractFilmData)
+			.then(buildResult)
 	}
 	catch (error) {
 		console.error(`An error occurred:`, error)
@@ -63,9 +59,76 @@ export async function findFilm(query: Query): Promise<Result> {
 }
 
 /**
+ * Parses out the film data of the first film in the poster grid. Expects markup like:
+ * ```
+ * <div class="poster-grid">
+ *     <ul>
+ *         <li class="tooltip griditem" data-original-title="The Spirit of the Beehive (1973)">
+ *             <div
+ *                 class="react-component"
+ *                 data-component-class="LazyPoster"
+ *                 data-request-poster-metadata="true"
+ *                 data-likeable="true"
+ *                 data-watchable="true"
+ *                 data-rateable="true"
+ *                 data-image-width="150"
+ *                 data-image-height="225"
+ *                 data-item-name="The Spirit of the Beehive (1973)"
+ *                 data-item-slug="the-spirit-of-the-beehive"
+ *                 data-item-link="/film/the-spirit-of-the-beehive/"
+ *                 data-item-full-display-name="The Spirit of the Beehive (1973)"
+ *                 data-film-id="49296"
+ *                 data-empty-poster-src="https://s.ltrbxd.com/static/img/empty-poster-150-DtnLDE3k.png"
+ *                 data-is-linked="true"
+ *                 data-target-link="/film/the-spirit-of-the-beehive/"
+ *                 data-details-endpoint="/film/the-spirit-of-the-beehive/json/"
+ *                 data-show-menu="true"
+ *                 data-hide-tooltip="true"
+ *             >
+ *             </div>
+ *         </li>
+ *         ...
+ *     </ul>
+ * </div>
+ * ```
+ */
+function extractFilmData(queryDocument: cheerio.CheerioAPI) {
+	const data = queryDocument('.poster-grid li div').attr()
+	return {
+		slug: data['data-item-slug'],
+		name: data['data-item-full-display-name'],
+		id: data['data-film-id'],
+	}
+}
+
+function buildResult(film: ReturnType<typeof extractFilmData>): Result {
+	return {
+		status: 'success',
+		title: film.name,
+		url: `https://letterboxd.com/film/${film.slug}`,
+		img: buildImageUrl(film),
+	}
+}
+
+function buildImageUrl({slug, id}: ReturnType<typeof extractFilmData>) {
+	// NOTE: there is a bug where sometimes the poster doesn't load. This is (often)
+	// because the slug used in the film url differs slightly from the one used on
+	// the image. For example:
+	// - https://a.ltrbxd.com/resized/film-poster/8/9/8/0/6/6/898066-close-your-eyes-0-460-0-690-crop.jpg
+	// - https://a.ltrbxd.com/resized/film-poster/8/9/8/0/6/6/898066-close-your-eyes-2023-0-460-0-690-crop.jpg
+	//
+	// The first one works (is Letterboxd's) but drops the `2023` from the films
+	// slug. The second one is how we would generate the url and does not work.
+	// I can't find any data on the page that reveals what this data is, and the
+	// images are now all lazy loaded. Perhaps we could try cutting off the year
+	// if the inital image 404s?
+	return `https://a.ltrbxd.com/resized/film-poster/${id.split('').join('/')}/${id}-${slug}-0-460-0-690-crop.jpg`
+}
+
+/**
  * Gets a random item from the given list.
  */
-function pickRandom(options: string[]) {
+function pickRandom<T>(options: T[]): T {
 	if (options.length == 1) {
 		return options[0]
 	}
